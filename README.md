5d13cd74f41d7a11700ebc67afabab901ec760bc
Jeremy Gonyea <jeremy.gonyea@gmail.com>
2017-06-26 13:44:55 -0400

    Initial project file commit

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# grav-bibleref
-Provides a field for bible references
+# Bibleref Plugin
+
+The **Bibleref** Plugin is for [Grav CMS](http://github.com/getgrav/grav). It adds fields to the header (and admin form) for Bible references by version, book, and chapter.
+
+## Installation
+
+Installing the Bibleref plugin can be done in one of two ways. The GPM (Grav Package Manager) installation method enables you to quickly and easily install the plugin with a simple terminal command, while the manual method enables you to do so via a zip file.
+
+### GPM Installation (Preferred)
+
+The simplest way to install this plugin is via the [Grav Package Manager (GPM)](http://learn.getgrav.org/advanced/grav-gpm) through your system's terminal (also called the command line).  From the root of your Grav install type:
+
+    bin/gpm install bibleref
+
+This will install the Bibleref plugin into your `/user/plugins` directory within Grav. Its files can be found under `/your/site/grav/user/plugins/bibleref`.
+
+### Manual Installation
+
+To install this plugin, just download the zip version of this repository and unzip it under `/your/site/grav/user/plugins`. Then, rename the folder to `bibleref`. You can find these files on [GitHub](https://github.com/jeremy-gonyea/grav-plugin-bibleref) or via [GetGrav.org](http://getgrav.org/downloads/plugins#extras).
+
+You should now have all the plugin files under
+
+    /your/site/grav/user/plugins/bibleref
+	
+> NOTE: This plugin is a modular component for Grav which requires [Grav](http://github.com/getgrav/grav) and the [Error](https://github.com/getgrav/grav-plugin-error) and [Problems](https://github.com/getgrav/grav-plugin-problems) to operate.
+
+## Configuration
+
+Currently, there are no configuration options.
+
+## Usage
+
+### Adding a reference via Admin
+Add the following code to your theme to add the fields to your content type 'TYPE':
+```
+
+  /**
+   * Extend page blueprints with bibleref fields.
+   *
+   * @param Event $event
+   */
+  public function onBlueprintCreated(Event $event)
+  {
+    static $inEvent = false;
+
+    /** @var Data\Blueprint $blueprint */
+    $blueprint = $event['blueprint'];
+    if (!$inEvent && $blueprint->getFilename() == 'TYPE') {
+      $inEvent = true;
+      $blueprints = new Data\Blueprints('user/plugins/bibleref/blueprints/');
+      $extends = $blueprints->get('bibleref');
+      $blueprint->extend($extends, true);
+      $inEvent = false;
+    }
+  }
+```
+Add Bible references via the fields and save your content.
+![Admin Fields](bibleref.png "Admin Fields")
+
+### Adding a reference via Markdown
+Add a Bible references to header similar to below:
+```
+title: New Page Title
+bible_references:
+    -
+        version: ESV
+        book: Genesis
+        chapter: '1'
+    -
+        version: KJV
+        book: Matthew
+        chapter: '1'
+---
+```
+ 
+### Displaying references
+To display all references on a page in your theme, use the following in your theme's template:
+
+`{% include 'partials/bibleref.html.twig' %}`
+
+and the page will display a div with class "bible-references" containing urls to BibleGateway.com with the selected references.
+
+## Credits
+
+This project was inspired by Bible Field from Drupal found [here](https://www.drupal.org/project/bible_field).
+
+## To Do
+
+- Add a default version to the plugin, but allow override per page.
+- Add more available translation versions from BibleGateway.com
---
+++ bibleref.php
@@ -0,0 +1,136 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\File\File;
+use Grav\Common\Grav;
+use RocketTheme\Toolbox\Event\Event;
+use Symfony\Component\Yaml\Yaml;
+
+
+/**
+ * Class BiblerefPlugin
+ * @package Grav\Plugin
+ */
+class BiblerefPlugin extends Plugin
+{
+  /**
+   * @return array
+   *
+   * The getSubscribedEvents() gives the core a list of events
+   *     that the plugin wants to listen to. The key of each
+   *     array section is the event that the plugin listens to
+   *     and the value (in the form of an array) contains the
+   *     callable (or function) as well as the priority. The
+   *     higher the number the higher the priority.
+   */
+  public static function getSubscribedEvents()
+  {
+    return [
+        'onPluginsInitialized' => ['onPluginsInitialized', 0],
+    ];
+  }
+
+  /**
+   * Initialize the plugin
+   */
+  public function onPluginsInitialized()
+  {
+
+    $this->copyInitData();
+
+
+    // Don't proceed if we are in the admin plugin
+    if ($this->isAdmin()) {
+      return;
+    }
+
+    // Enable the main event we are interested in
+    $this->enable([
+      'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+    ]);
+  }
+
+  /**
+   * Add current directory to twig lookup paths.
+   */
+  public function onTwigTemplatePaths()
+  {
+    $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+  }
+
+
+  /**
+   * Returns listing of available books from a yaml data file.
+   *
+   * @return array
+   */
+  public static function getBookOptions()
+  {
+    $options_array = array();
+    $book_list_path = DATA_DIR . 'bibleref/book_data.yaml';
+    $book_list_file = File::instance($book_list_path);
+    if(!file_exists($book_list_path)){
+      BiblerefPlugin::copyInitData();
+    }
+    $sections = Yaml::parse($book_list_file->content());
+
+    foreach ($sections as $section){
+      foreach ($section as $book_meta){
+        $options_array[$book_meta['book']['name']] = $book_meta['book']['name'];
+      }
+    }
+    return $options_array;
+  }
+
+  /**
+   * Returns listing of available chapters based on a selected book.
+   *
+   * @param string $book
+   *  Book title to find number of chapters of.
+   * @return array
+   */
+  public static function getBookChapterOptions($book_selected = "Psalms")
+  {
+    $max_chapter = 1;
+    $options_array = array();
+    $book_list_path = DATA_DIR . 'bibleref/book_data.yaml';
+    $book_list_file = File::instance($book_list_path);
+    if(!file_exists($book_list_path)){
+      BiblerefPlugin::copyInitData();
+    }
+    $sections = Yaml::parse($book_list_file->content());
+
+    // Search each section for book.
+    foreach ($sections as $section){
+      foreach ($section as $book_meta)
+        if ($book_meta['book']['name'] == $book_selected){
+          $max_chapter = $book_meta['book']['chapters'];
+        }
+    }
+
+    for($i = 1; $i <= $max_chapter; $i++){
+      $options_array[$i] = $i;
+    }
+
+    return $options_array;
+  }
+
+  public static function copyInitData(){
+    $grav = new Grav();
+    $init_data_file_path = __DIR__ . '/data/book_chapters.yaml';
+    $user_data_file_path = DATA_DIR . '/bibleref/book_data.yaml';
+
+    // No need to copy if file already exists.
+    if(file_exists($user_data_file_path)){
+      return;
+    }
+
+    $init_file = File::instance($init_data_file_path);
+    $user_file = File::instance($user_data_file_path);
+    $data = Yaml::parse($init_file->content());
+    $user_file->save(Yaml::dump($data));
+    $grav::instance()['log']->notice('Copied bible book data to ' . DATA_DIR . 'bibleref/book_data.yaml' );
+  }
+
+}
